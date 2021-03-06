51fa710f638a1572e35a671d99f58e549cf88fb5
David Ten Sing <david10sing@hotmail.com>
2021-03-06 20:37:51 +0400

    v1.1.1

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import ScrollspyIO from './react-scrollspy-io';
 import Spy, { SpyItem } from './components/spy';
 import SpyOn from './components/spyOn';
 
-export default ScrollspyIO;
 export { Spy, SpyItem, SpyOn };
+export default ScrollspyIO;
